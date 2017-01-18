871f6b3183895f321547a0831209f4785061a9b7
usrmnw <mayureshwaykole@gmail.com>
2017-01-19 01:10:25 +0530

    fixed accordion

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
+// $(document).ready(function() {
+//   $('.collapse.in').prev('.panel-heading').addClass('active');
+//   $('#accordion, #bs-collapse')
+//     .on('show.bs.collapse', function(a) {
+//       $(a.target).prev('.panel-heading').addClass('active');
+//     })
+//     .on('hide.bs.collapse', function(a) {
+//       $(a.target).prev('.panel-heading').removeClass('active');
+//     });
+//   });
+
 $(document).ready(function() {
   $('.collapse.in').prev('.panel-heading').addClass('active');
-  $('#accordion, #bs-collapse')
+  $('#accordion1')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
+
+  $('#accordion2')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
+  $('#accordion3')
     .on('show.bs.collapse', function(a) {
       $(a.target).prev('.panel-heading').addClass('active');
     })
